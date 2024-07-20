4e6bc44c3ede83afb701513a48488e932612062e
akramexp <akram.exp02@gmail.com>
2024-07-20 18:47:50 +0530

    added event category and order models

---
+++ lib/database/models/category.model.ts
@@ -0,0 +1,13 @@
+import mongoose, { models, Schema } from "mongoose";
+
+export interface ICategory extends Document {
+  _id: string;
+  name: string;
+}
+
+const categorySchema = new Schema({
+  name: { type: String, required: true, unique: true },
+});
+
+export const Category =
+  models.Category || mongoose.model("Category", categorySchema);
