140b2467908ad4c1a02cb982f7fa5b0755b33c25
akramexp <akram.exp02@gmail.com>
2024-08-02 23:23:12 +0530

    minor changes

--- components/shared/Checkout.tsx
+++ components/shared/Checkout.tsx
@@ -2,10 +2,25 @@ import { IEvent2 } from "@/types";
 import { Button } from "../ui/button";
 import { loadStripe } from "@stripe/stripe-js";
 import { checkoutOrder } from "@/lib/services/order";
+import { useEffect } from "react";
 
 loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
 
 const Checkout = ({ event, userId }: { event: IEvent2; userId: string }) => {
+  useEffect(() => {
+    // Check to see if this is a redirect back from Checkout
+    const query = new URLSearchParams(window.location.search);
+    if (query.get("success")) {
+      console.log("Order placed! You will receive an email confirmation.");
+    }
+
+    if (query.get("canceled")) {
+      console.log(
+        "Order canceled -- continue to shop around and checkout when you’re ready."
+      );
+    }
+  }, []);
+
   const onCheckout = async () => {
     const order = {
       eventTitle: event.title,
@@ -18,7 +33,7 @@ const Checkout = ({ event, userId }: { event: IEvent2; userId: string }) => {
     await checkoutOrder(order);
   };
   return (
-    <form action={onCheckout}>
+    <form action={onCheckout} method="POST">
       <Button type="submit" role="link" size="lg" className="button sm:w-fit">
         {event.isFree ? "Get Ticket" : "Buy Ticket"}
       </Button>
